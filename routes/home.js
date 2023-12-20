54ef8446acd8bd383055cf799ead11507a451079
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-12-19 22:39:35 -0300

    categorias traida, falta interactuar con ellas

--- routes/home.js
+++ routes/home.js
@@ -5,18 +5,20 @@ var util = require('util');
 var cloudinary = require('cloudinary').v2;
 const uploader = util.promisify(cloudinary.uploader.upload);
 var movimientosService = require('../service/movimientosService')
+var categoriasService = require('../service/categoriaService')
 
 router.get('/',async function(req, res, next) {
     const currentDate = new Date();
     const currentMonth = currentDate.getMonth() + 1;
     const currentYear = currentDate.getFullYear();
     var movimientos = await movimientosService.getMovimientosByUserIdAndMes(res.locals.id_usuario,currentMonth, currentYear)
-    console.log(movimientos.length)
+    var categorias = await categoriasService.getCategoriasByUserId(res.locals.id_usuario)
     res.render('home/home', {
         layout: 'layout',
         movimientos: movimientos,
         currentMonth: currentMonth,
         currentYear: currentYear,
+        categorias: categorias
     });
 });
 
